55dac5f9d50f45fd59447ce42aa4577b63b1dcb6
tharaka-ariyarathna <tharaka.ariyarathna@velaris.io>
2024-01-14 00:16:56 +0530

    feat: profile pages

--- src/components/profileCard.jsx
+++ src/components/profileCard.jsx
@@ -18,7 +18,7 @@ const defaultTheme = createTheme();
 
 export const ProfileCard = ({ profileType }) => {
   const profile = {
-    type: "student",
+    type: "teacher",
     name: "Anne Bonny",
     data1: "Learning front-end is damn hard",
     data2:
@@ -33,65 +33,69 @@ export const ProfileCard = ({ profileType }) => {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Stack direction="row" spacing={1} sx={{ backgroundColor: "#ecedeb" }}>
-        <Box sx={{ height: "100vh", width: "40%" }} justifyContent="center">
+      <Stack direction='row' spacing={1} sx={{ backgroundColor: '#ecedeb' }}>
+        <Box
+          className='profileSide'
+          sx={{ height: '100vh', width: '40%' }}
+          justifyContent='center'
+        >
           <Grid
             container
             spacing={0}
-            direction="column"
-            alignItems="center"
-            justify="center"
-            style={{ minHeight: "80vh" }}
+            direction='column'
+            alignItems='center'
+            justify='center'
+            style={{ minHeight: '80vh' }}
             marginTop={3}
           >
             <Card
               sx={{
                 width: 400,
-                maxWidth: "100%",
-                boxShadow: "lg",
-                height: "83vh",
-                backgroundColor: "#158ced",
-                color: "#e9fae1",
+                maxWidth: '100%',
+                boxShadow: 'lg',
+                height: '83vh',
+                backgroundColor: '#158ced',
+                color: '#e9fae1',
               }}
             >
-              <CardContent sx={{ alignItems: "center", textAlign: "center" }}>
+              <CardContent sx={{ alignItems: 'center', textAlign: 'center' }}>
                 <img
-                  src="img/userProfile.jpg"
-                  style={{ height: "200", borderRadius: "50%" }}
-                  alt="Profile"
+                  src='img/userProfile.jpg'
+                  style={{ height: '200', borderRadius: '50%' }}
+                  alt='Profile'
                 />
                 <Typography
-                  level="title-lg"
-                  variant="h4"
-                  sx={{ padding: 2, color: "#bfd6b4" }}
+                  level='title-lg'
+                  variant='h4'
+                  sx={{ padding: 2, color: '#bfd6b4' }}
                 >
                   {profile.name}
                 </Typography>
-                <Typography level="body-sm" sx={{ padding: 1 }}>
+                <Typography level='body-sm' fontSize={12} sx={{ padding: 1 }}>
                   {profile.data1}
                 </Typography>
-                <Typography level="body-sm" sx={{ padding: 1 }}>
+                <Typography level='body-sm' fontSize={12} sx={{ padding: 1 }}>
                   {profile.data2}
                 </Typography>
                 <Box
                   sx={{
-                    display: "flex",
+                    display: 'flex',
                     gap: 2,
                     mt: 2,
                   }}
                 >
-                  <Box justifyContent="start">
-                    <Typography variant="h6" sx={{ color: "#bfd6b4" }}>
+                  <Box justifyContent='start'>
+                    <Typography variant='h5' sx={{ color: '#bfd6b4' }}>
                       User Information
                     </Typography>
-                    <List marker="User Information">
+                    <List marker='User Information'>
                       <ListItem>
-                        <Typography variant="body1">
+                        <Typography fontSize={12} variant='body1'>
                           Email - {profile.email}
                         </Typography>
                       </ListItem>
-                      <ListItem variant="p">
-                        <Typography variant="body1">
+                      <ListItem variant='p'>
+                        <Typography fontSize={12} variant='body1'>
                           Country - {profile.country}
                         </Typography>
                       </ListItem>
@@ -102,8 +106,8 @@ export const ProfileCard = ({ profileType }) => {
             </Card>
           </Grid>
         </Box>
-        <Box sx={{ height: "100vh", width: "65%" }}>
-          {profile.type === "student" ? (
+        <Box sx={{ height: '100vh', width: '65%' }}>
+          {profile.type === 'student' ? (
             <StudentProfile
               games={games}
               badges={badges}
