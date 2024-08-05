9d47312b51415d2be36fe0d518cef42330363383
Supuni Sithara <sitharabandara1997@gmail.com>
2024-08-05 18:50:45 +0530

    Updated game window

--- src/components/UnityGame.js
+++ src/components/UnityGame.js
@@ -2,13 +2,20 @@ import React from 'react';
 
 const UnityGame = () => {
   return (
-    <div>
-     
+    <div style={{ overflow: 'hidden', width: '100%', height: '600px', position: 'relative' }}>
       <iframe
         src="https://heroic-dusk-6d491e.netlify.app/"
-        width="100%"
-        height="600px"
         title="Unity WebGL Game"
+        style={{
+          border: 'none',
+          transform: 'scale(1)', // No scaling, original size
+          transformOrigin: 'top left', // Adjust the origin if you scale
+          width: '100%', // Full width of the parent div
+          height: '100%', // Full height of the parent div
+          position: 'absolute',
+          top: 0,
+          left: 0,
+        }}
       />
     </div>
   );
